8bf6970b5f614118d2debba9a434b36e98621ae5
AapkaGitHubName <aapkaemail@example.com>
2026-08-31 14:38:09 +0530

    ksnhx

--- next.config.ts
+++ next.config.ts
@@ -50,7 +50,7 @@ function createNextConfig(phase: string): NextConfig {
 
   return {
     /* config options here */
-    output: "standalone",
+    output: process.env.VERCEL ? undefined : "standalone",
     deploymentId,
     env: {
       NEXT_PUBLIC_DEPLOYMENT_ID: deploymentId,
